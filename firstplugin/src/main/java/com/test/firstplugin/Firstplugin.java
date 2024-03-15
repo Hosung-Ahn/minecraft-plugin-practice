da59dba5dd00fbfb86f17827f52956f2f1c25f4f
HOSUNG <an3735297@pusan.ac.kr>
2024-03-15 22:34:39 +0900

    ItemStack, ItemMeta

--- firstplugin/src/main/java/com/test/firstplugin/Firstplugin.java
+++ firstplugin/src/main/java/com/test/firstplugin/Firstplugin.java
@@ -2,10 +2,18 @@ package com.test.firstplugin;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreeperPowerEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.player.PlayerEggThrowEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Firstplugin extends JavaPlugin implements Listener {
@@ -36,5 +44,22 @@ public final class Firstplugin extends JavaPlugin implements Listener {
     public void onPlayerEggThrow(PlayerEggThrowEvent event) {
         event.setHatching(false);
         event.getPlayer().sendMessage(ChatColor.RED + "You can't throw eggs!");
+    }
+
+    @EventHandler
+    public void onEntitySpawn(CreeperPowerEvent event) {
+        ItemStack item = new ItemStack(Material.DIAMOND);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(ChatColor.RED + "Explosive Diamond");
+        item.setItemMeta(meta);
+
+        Bukkit.getWorld("world").getBlockAt(new Location(Bukkit.getWorld("world"), 0, 0, 0)).setType(Material.DIAMOND_BLOCK);
+
+        ItemStack book = new ItemStack(Material.BOOK);
+        BookMeta bookMeta = (BookMeta) book.getItemMeta();
+        bookMeta.setAuthor("Notch");
+        bookMeta.setTitle("The Book of Notch");
+        bookMeta.setPages("Page 1", "Page 2", "Page 3");
+        book.setItemMeta(bookMeta);
     }
 } 
