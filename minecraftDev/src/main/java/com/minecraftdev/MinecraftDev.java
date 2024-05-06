1448d479b61493c365c6321050b35f54ec1f1d88
Hosung <an3735297@naver.com>
2024-05-06 20:02:00 +0900

    message system : apply command

--- minecraftDev/src/main/java/com/minecraftdev/MinecraftDev.java
+++ minecraftDev/src/main/java/com/minecraftdev/MinecraftDev.java
@@ -4,9 +4,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MinecraftDev extends JavaPlugin {
 
+    // /message {player name} {message}
+    // /reply {message}
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getCommand("message").setExecutor(new MessageCommand());
+        getCommand("reply").setExecutor(new ReplyCommand());
     }
 
     @Override
