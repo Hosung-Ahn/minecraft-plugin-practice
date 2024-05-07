2f002eaecb1ac18b164d22aea6e6ec875191a57e
Hosung <an3735297@naver.com>
2024-05-07 22:37:18 +0900

    create new project : spigot advance

---
+++ spigotAdvance/src/main/java/com/spigotadvance/SpigotAdvance.java
@@ -0,0 +1,17 @@
+package com.spigotadvance;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpigotAdvance extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
