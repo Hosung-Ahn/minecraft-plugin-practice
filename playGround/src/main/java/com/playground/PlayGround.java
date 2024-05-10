f09080a875c3022aa61fe908d2dd9065a0a75dc4
Hosung <an3735297@naver.com>
2024-05-11 05:43:53 +0900

    refactor file path

--- playGround/src/main/java/com/playground/PlayGround.java
+++ playGround/src/main/java/com/playground/PlayGround.java
@@ -1,5 +1,7 @@
 package com.playground;
 
+import com.playground.commandTabComplete.FruitCommand;
+import com.playground.commandTabComplete.FruitTab;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PlayGround extends JavaPlugin {
--- playGround/src/main/java/com/playground/commandTabComplete/FruitCommand.java
+++ playGround/src/main/java/com/playground/commandTabComplete/FruitCommand.java
@@ -1,4 +1,4 @@
-package com.playground;
+package com.playground.commandTabComplete;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
