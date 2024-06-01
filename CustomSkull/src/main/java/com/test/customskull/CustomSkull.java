82914f83f4775f225e7749d500099f209eb82723
HOSUNG <an3735297@pusan.ac.kr>
2024-06-01 21:42:25 +0900

    customSkull : 프로젝트 생성

---
+++ CustomSkull/src/main/java/com/test/customskull/CustomSkull.java
@@ -0,0 +1,17 @@
+package com.test.customskull;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomSkull extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
