8f9ce47f8fff295872d99f094f39e433bf093abb
HOSUNG <an3735297@pusan.ac.kr>
2024-06-04 19:38:54 +0900

    Custom File : 프로젝트 생성

---
+++ customFile/src/main/java/com/test/customfile/CustomFile.java
@@ -0,0 +1,17 @@
+package com.test.customfile;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomFile extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
