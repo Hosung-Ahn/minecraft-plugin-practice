adbeb96a5b2e96186d080c0dcca7c9a02a390a83
HOSUNG <an3735297@pusan.ac.kr>
2024-06-22 00:48:51 +0900

    create project

---
+++ Etc/src/main/java/com/test/etc/Etc.java
@@ -0,0 +1,17 @@
+package com.test.etc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Etc extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
