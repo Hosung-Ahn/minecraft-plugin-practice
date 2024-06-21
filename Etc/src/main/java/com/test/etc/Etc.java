652df06c6f719c447e0588d7fcbd3e40bca22820
HOSUNG <an3735297@pusan.ac.kr>
2024-06-22 01:13:32 +0900

    persistence data container

--- Etc/src/main/java/com/test/etc/Etc.java
+++ Etc/src/main/java/com/test/etc/Etc.java
@@ -1,13 +1,30 @@
 package com.test.etc;
 
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Etc extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        // persistence data container
+        NamespacedKey key = new NamespacedKey(this, "name");
 
+        Player player = Bukkit.getPlayer("player");
+        player.getPersistentDataContainer().set(key, PersistentDataType.STRING, "value");
+
+        // key 에 해당하는 value 가 없다면 null 을 반환하므로 null 체크를 해야 한다.
+        if (player.getPersistentDataContainer().has(key, PersistentDataType.STRING)) {
+            String value = player.getPersistentDataContainer().get(key, PersistentDataType.STRING);
+        }
+
+        Block block = Bukkit.getWorld("world").getBlockAt(0, 0, 0);
+        Sign
     }
 
     @Override
